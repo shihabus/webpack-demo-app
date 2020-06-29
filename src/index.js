cc3d6fd7fddbba6ae8eb5ec9a45b7167afc32232
Shihab US <shihabudheen@dunzo.in>
2020-06-29 14:14:54 +0530

    adding scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { run } from "./app/app";
 
 import "./main.css";
+import "./main.scss";
+
 import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/components.service";
 
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,16 @@ module.exports = {
   module: {
     // loaders load in reverse order
     // first it will pass the file through css-loader then pass it to style-loader
-    rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }],
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // 3. Inject styles into DOM
+          "css-loader", // 2. css into common js
+          "sass-loader" // 1. Turn sass into css
+        ]
+      },
+    ],
   },
 };
